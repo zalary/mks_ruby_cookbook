3bc39ef71ed38ffa72454fab63f75128f5fe1d7e
Zalary Young <zalary@Zim.local>
2014-02-05 15:07:23 -0600

    through level 6

---
+++ cookbook.rb
@@ -0,0 +1,25 @@
+class Cookbook
+  attr_accessor :title
+  def initialize(title)
+    @title = title
+    @recipes = []
+  end
+
+  def recipes
+    @recipes    
+  end
+
+  def add_recipe(recipe_title)
+    @recipes << recipe_title
+  end
+
+ end
+
+class Recipe
+  attr_accessor :title, :ingredients, :steps
+  def initialize(title, ingredients, steps)
+    @title = title
+    @ingredients = ingredients
+    @steps = steps
+  end
+end
---
+++ testcode.rb
@@ -0,0 +1,25 @@
+require_relative 'cookbook'
+
+mex_cuisine = Cookbook.new("Mexican Cooking")
+
+burrito = Recipe.new("Bean Burrito", ["tortilla", "bean"], ["heat beans", "place beans in tortilla", "roll up"])
+
+puts mex_cuisine.title
+puts burrito.title # Bean Burrito
+p burrito.ingredients # ["tortilla", "bean", "cheese"]
+p burrito.steps # ["heat beans", "heat tortilla", "place beans in tortilla", "sprinkle cheese on top", "roll up"]
+
+mex_cuisine.title = "Mexican Recipes"
+puts mex_cuisine.title # Mexican Recipesi
+
+burrito.title = "Veggie Burrito"
+burrito.ingredients = ["tortilla", "tomatoes"]
+burrito.steps = ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
+
+p burrito.title # "Veggie Burrito"
+p burrito.ingredients # ["tortilla", "tomatoes"]
+p burrito.steps # ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
+
+mex_cuisine.recipes # []
+mex_cuisine.add_recipe(burrito)
+p mex_cuisine.recipes # [#<Recipe:0x007fbc3b92e560 @title="Veggie Burrito", @ingredients=["tortilla", "tomatoes"], @steps=["heat tomatoes", "place tomatoes in tortilla", "roll up"]>]
