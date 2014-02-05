d2cb1e0d4e8e50a3c85525f50c9050521c0e16da
Ian White <ianwhite@Ians-MacBook-Pro.local>
2014-02-05 16:24:41 -0600

    Finish ex 11 under classes.Begin adding comments

--- cookbook.rb
+++ cookbook.rb
@@ -1,5 +1,7 @@
 class Cookbook
-  attr_accessor :title
+  attr_accessor :title  # attr_accessor virtually creates def title=(new_title) so they may be accessed outside the class ie in Pry.
+                                              #             @title = new_title
+                                              #           end
   def initialize(title)
     @title = title
     @recipes = []
@@ -9,18 +11,35 @@ class Cookbook
     @recipes
   end
 
-  def add_recipe(recipe_title)
-    @recipes << recipe_title
-    puts "Added a recipe to the collection: #{recipe_title.title}"
+  # def add_recipe(recipe)  adds recipe class instance (with all its methods ie .title, .ingredients, .steps)
+  #                         to (cookbook) class variable @recipes
+    @recipes << recipe
+    puts "Added a recipe to the collection: #{recipe.title}"
+  end
+
+  def recipe_titles
+    @recipes.each { |recipe| puts recipe.title }
+  end
+
+  def recipe_ingredients
+    @recipes.each { |recipe| puts "These are the ingredients for the #{recipe.title}:  #{recipe.ingredients}" }
   end
 
  end
 
 class Recipe
+
   attr_accessor :title, :ingredients, :steps
+
   def initialize(title, ingredients, steps)
     @title = title
     @ingredients = ingredients
     @steps = steps
   end
+
+  def print_recipe
+    puts "Recipe info for #{@title}: Ingredients: #{@ingredients.join(", ")}.  Steps: #{@steps.join(", ")}"
+  end
+
 end
+
--- testcode.rb
+++ testcode.rb
@@ -4,10 +4,10 @@ mex_cuisine = Cookbook.new("Mexican Cooking")
 
 burrito = Recipe.new("Bean Burrito", ["tortilla", "bean"], ["heat beans", "place beans in tortilla", "roll up"])
 
-puts mex_cuisine.title
-puts burrito.title # Bean Burrito
-p burrito.ingredients # ["tortilla", "bean", "cheese"]
-p burrito.steps # ["heat beans", "heat tortilla", "place beans in tortilla", "sprinkle cheese on top", "roll up"]
+# puts mex_cuisine.title
+# puts burrito.title # Bean Burrito
+# p burrito.ingredients # ["tortilla", "bean", "cheese"]
+# p burrito.steps # ["heat beans", "heat tortilla", "place beans in tortilla", "sprinkle cheese on top", "roll up"]
 
 mex_cuisine.title = "Mexican Recipes"
 puts mex_cuisine.title # Mexican Recipesi
@@ -16,10 +16,15 @@ burrito.title = "Veggie Burrito"
 burrito.ingredients = ["tortilla", "tomatoes"]
 burrito.steps = ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
 
-p burrito.title # "Veggie Burrito"
-p burrito.ingredients # ["tortilla", "tomatoes"]
-p burrito.steps # ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
+# p burrito.title # "Veggie Burrito"
+# p burrito.ingredients # ["tortilla", "tomatoes"]
+# p burrito.steps # ["heat tomatoes", "place tomatoes in tortilla", "roll up"]
 
-mex_cuisine.recipes # []
+# mex_cuisine.recipes # []
 mex_cuisine.add_recipe(burrito)
-p mex_cuisine.recipes # [#<Recipe:0x007fbc3b92e560 @title="Veggie Burrito", @ingredients=["tortilla", "tomatoes"], @steps=["heat tomatoes", "place tomatoes in tortilla", "roll up"]>]
+# p mex_cuisine.recipes # [#<Recipe:0x007fbc3b92e560 @title="Veggie Burrito", @ingredients=["tortilla", "tomatoes"], @steps=["heat tomatoes", "place tomatoes in tortilla", "roll up"]>]
+
+mex_cuisine.recipe_titles # Veggie Burrito
+mex_cuisine.recipe_ingredients # These are the ingredients for Veggie Burrito: ["tortilla", "bean"]
+
+pie = Recipe.new("Yum Pie", ["flour", "water"], ["mix", "heat", "done"])
